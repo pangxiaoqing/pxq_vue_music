f5b3a813a23d7712c5a5d82f64fb3ebabdbc9bcd
pangxiaoqing <xiaoqing246810@163.com>
2018-06-08 18:12:20 +0800

    add

---
+++ src/common/js/config.js
@@ -0,0 +1,6 @@
+/*配置文件-项目相关的信息*/
+export const playMode = {
+  sequence: 0, /*顺序播放*/
+  loop: 1, /*循环播放*/
+  random: 2 /*随机播放*/
+}
--- src/store/state.js
+++ src/store/state.js
@@ -1,14 +1,14 @@
-// import { playMode } from 'common/js/config'
+import { playMode } from 'common/js/config'
 // import { loadSearch } from 'common/js/cache'
 
 const state = {
-  singer: {}
-  // playing: false,
-  // fullScreen: false,
-  // playlist: [],
-  // sequenceList: [],
-  // mode: playMode.sequence,
-  // currentIndex: -1,
+  singer: {},
+  playing: false, /*播放*/
+  fullScreen: false, /*全屏*/
+  playlist: [], /*播放列表*/
+  sequenceList: [], /*顺序列表*/
+  mode: playMode.sequence, /*播放模式*/
+  currentIndex: -1 /*当前播放的索引*/
   // disc: {},
   // topList: {},
   // searchHistory: loadSearch(),
